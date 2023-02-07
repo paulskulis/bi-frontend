7cc82c5418fc1f315f3d90dba35aee10ab809264
paulskulis <pauls@pauls.fyi>
2023-02-07 20:56:40 +0200

    Updated product page design and node dependencies

--- tpl/product.php
+++ tpl/product.php
@@ -7,15 +7,100 @@
 		</div>
 		<div class="col-md-6">
 			<h1>APS - caurule L-500</h1>
-			<span class="text-secondary">Preces kods: XXXX</span>
-			<p class="mt-4 text-secondary">SIA LLP ir radījis produktu APS (Abrazīvā Cauruļu Sistēma).</p>
-			<p class="mt-4 text-secondary">APS sistēma ir vienkārši un ērti uzstādāma. APS caurules kalpos desmit reizes ilgāk par metāla graudu caurplūdes caurulēm.</p>
-			<p class="mt-4 text-secondary">APS sauklis ir “Gara mūža caurules”, jo pēc laika, kad plastmasa ir izdilusi, jānomaina ir tikai pati plastmasa, nevis viss komplekts.</p>
-			<p class="mt-4 text-secondary">Plastmasas nomaiņa ir tik vienkārša, ka klients to var izdarīt pats un bez speciālām palīgierīcēm.</p>
-			<p class="mt-4 text-body">Noliktavas atlikums:  <span class="text-body fw-bold">12</span></p>
-
-			<h2 class="fw-bold mt-2">126.00 €</h2>
-			<span>Cena bez PVN</span>
+			<span class="text-secondary fs-6">Preces kods: XXXX</span>
+			<form class="my-4 pt-2">
+				<div class="row mt-4">
+					<label for="select1" class="col-md-3 col-form-label">Ø, mm:</label>
+					<div class="col-md-2">
+						<select id="select1" class="form-select">
+							<option>705</option>
+							<option>123</option>
+						</select>
+					</div>
+				</div>
+				<div class="row mt-2">
+					<label for="select2" class="col-md-3 col-form-label">Garums, m:</label>
+					<div class="col-md-2">
+						<select id="select2" class="form-select">
+							<option>705</option>
+							<option>123</option>
+						</select>
+					</div>
+				</div>
+				<div class="row mt-2">
+					<label for="select3" class="col-md-3 col-form-label">kW:</label>
+					<div class="col-md-2">
+						<select id="select3" class="form-select">
+							<option>705</option>
+							<option>123</option>
+						</select>
+					</div>
+				</div>
+			</form>
+			<div class="my-4">
+				<h2 class="fw-bold pt-4">126.00 €</h2>
+				<span class="fs-6">853.05 € ar PVN</span><br>
+				<div class="btn-group mt-2 productCounter" role="group">
+					<button type="button text-secondary" class="btn btn-decrement btn-outline-secondary px-3 py-2 bg-light text-body border-0">-</button>
+					<button type="button" class="btn counter btn-outline-secondary disabled px-4 py-2 bg-light text-body border-0">1</button>
+					<button type="button" class="btn btn-increment btn-outline-secondary px-3 py-2 bg-light text-body border-0">+</button>
+					<button type="button" class="btn btn-warning py-2 px-4 text-body border-0 fw-bold w-100 addToCart" data-productId="123" data-bs-toggle="modal" data-bs-target="#cartModal"> <img src="assets/img/cart.svg" class="pe-4">Pievienot grozam</button>
+					<!--<button type="button" class="btn btn-light text-body toggleWishlist" data-productId="123"><div class="iconContainer" style="height:24px;width:24px"></div></button>-->
+				</div>
+			</div>
+			<h5 class="fw-bold text-uppercase my-4 pt-2">Lejupielādēt instrukciju</h5>
+			<div class="row">
+				<div class="col-md-3 mb-2">
+					<div class="input-group btn-group-lg d-grid" role="group" aria-label="Large button group">
+						<button type="button" class="btn btn-dark fw-bold rounded-pill px-4" onclick="window.open('https://www.africau.edu/images/default/sample.pdf','_blank');">
+							<svg class="float-start mt-1" width="22px" height="22px" viewBox="0 0 22 22" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+								<g transform="translate(-1204.000000, -1044.000000)" fill="#FFFFFF" fill-rule="nonzero">
+									<path d="M1214.54279,1061.92804 L1206.77797,1054.1637 C1206.52557,1053.91131 1206.52557,1053.50123 1206.77797,1053.24883 C1207.03038,1052.99645 1207.44048,1052.99645 1207.69289,1053.24883 L1214.35399,1059.90787 L1214.35399,1044.63928 C1214.35399,1044.28724 1214.65428,1044 1215.00114,1044 C1215.3584,1044 1215.64828,1044.29246 1215.64828,1044.63928 L1215.64828,1059.90898 L1222.31049,1053.24828 C1222.56419,1052.99589 1222.973,1052.99589 1223.22541,1053.24828 C1223.47911,1053.50067 1223.47911,1053.91075 1223.22541,1054.16314 L1215.45782,1061.92749 C1215.20542,1062.17987 1214.79531,1062.17987 1214.5429,1061.92749 L1214.54279,1061.92804 Z M1225.36584,1064.70579 C1225.7166,1064.70579 1226,1065.00606 1226,1065.3529 C1226,1065.71013 1225.72182,1066 1225.36584,1066 L1204.63544,1065.99978 C1204.28468,1065.99978 1204,1065.69952 1204,1065.35268 C1204,1064.99544 1204.27818,1064.70558 1204.63544,1064.70558 L1225.36584,1064.70579 Z" id="Shape"></path>
+								</g>
+							</svg>
+							<span class="ps-4 fs-6">LV</span>
+						</button>
+					</div>
+				</div>
+				<div class="col-md-3 mb-2">
+					<div class="input-group btn-group-lg d-grid" role="group" aria-label="Large button group">
+						<button type="button" class="btn btn-dark fw-bold rounded-pill px-4" onclick="window.open('https://www.africau.edu/images/default/sample.pdf','_blank');">
+							<svg class="float-start mt-1" width="22px" height="22px" viewBox="0 0 22 22" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+								<g transform="translate(-1204.000000, -1044.000000)" fill="#FFFFFF" fill-rule="nonzero">
+									<path d="M1214.54279,1061.92804 L1206.77797,1054.1637 C1206.52557,1053.91131 1206.52557,1053.50123 1206.77797,1053.24883 C1207.03038,1052.99645 1207.44048,1052.99645 1207.69289,1053.24883 L1214.35399,1059.90787 L1214.35399,1044.63928 C1214.35399,1044.28724 1214.65428,1044 1215.00114,1044 C1215.3584,1044 1215.64828,1044.29246 1215.64828,1044.63928 L1215.64828,1059.90898 L1222.31049,1053.24828 C1222.56419,1052.99589 1222.973,1052.99589 1223.22541,1053.24828 C1223.47911,1053.50067 1223.47911,1053.91075 1223.22541,1054.16314 L1215.45782,1061.92749 C1215.20542,1062.17987 1214.79531,1062.17987 1214.5429,1061.92749 L1214.54279,1061.92804 Z M1225.36584,1064.70579 C1225.7166,1064.70579 1226,1065.00606 1226,1065.3529 C1226,1065.71013 1225.72182,1066 1225.36584,1066 L1204.63544,1065.99978 C1204.28468,1065.99978 1204,1065.69952 1204,1065.35268 C1204,1064.99544 1204.27818,1064.70558 1204.63544,1064.70558 L1225.36584,1064.70579 Z" id="Shape"></path>
+								</g>
+							</svg>
+							<span class="ps-4 fs-6">LT</span>
+						</button>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-3 mb-2">
+					<div class="input-group btn-group-lg d-grid" role="group" aria-label="Large button group">
+						<button type="button" class="btn btn-dark fw-bold rounded-pill px-4" onclick="window.open('https://www.africau.edu/images/default/sample.pdf','_blank');">
+							<svg class="float-start mt-1" width="22px" height="22px" viewBox="0 0 22 22" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+								<g transform="translate(-1204.000000, -1044.000000)" fill="#FFFFFF" fill-rule="nonzero">
+									<path d="M1214.54279,1061.92804 L1206.77797,1054.1637 C1206.52557,1053.91131 1206.52557,1053.50123 1206.77797,1053.24883 C1207.03038,1052.99645 1207.44048,1052.99645 1207.69289,1053.24883 L1214.35399,1059.90787 L1214.35399,1044.63928 C1214.35399,1044.28724 1214.65428,1044 1215.00114,1044 C1215.3584,1044 1215.64828,1044.29246 1215.64828,1044.63928 L1215.64828,1059.90898 L1222.31049,1053.24828 C1222.56419,1052.99589 1222.973,1052.99589 1223.22541,1053.24828 C1223.47911,1053.50067 1223.47911,1053.91075 1223.22541,1054.16314 L1215.45782,1061.92749 C1215.20542,1062.17987 1214.79531,1062.17987 1214.5429,1061.92749 L1214.54279,1061.92804 Z M1225.36584,1064.70579 C1225.7166,1064.70579 1226,1065.00606 1226,1065.3529 C1226,1065.71013 1225.72182,1066 1225.36584,1066 L1204.63544,1065.99978 C1204.28468,1065.99978 1204,1065.69952 1204,1065.35268 C1204,1064.99544 1204.27818,1064.70558 1204.63544,1064.70558 L1225.36584,1064.70579 Z" id="Shape"></path>
+								</g>
+							</svg>
+							<span class="ps-4 fs-6">RU</span>
+						</button>
+					</div>
+				</div>
+				<div class="col-md-3 mb-2">
+					<div class="input-group btn-group-lg d-grid" role="group" aria-label="Large button group">
+						<button type="button" class="btn btn-dark fw-bold rounded-pill px-4" onclick="window.open('https://www.africau.edu/images/default/sample.pdf','_blank');">
+							<svg class="float-start mt-1" width="22px" height="22px" viewBox="0 0 22 22" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+								<g transform="translate(-1204.000000, -1044.000000)" fill="#FFFFFF" fill-rule="nonzero">
+									<path d="M1214.54279,1061.92804 L1206.77797,1054.1637 C1206.52557,1053.91131 1206.52557,1053.50123 1206.77797,1053.24883 C1207.03038,1052.99645 1207.44048,1052.99645 1207.69289,1053.24883 L1214.35399,1059.90787 L1214.35399,1044.63928 C1214.35399,1044.28724 1214.65428,1044 1215.00114,1044 C1215.3584,1044 1215.64828,1044.29246 1215.64828,1044.63928 L1215.64828,1059.90898 L1222.31049,1053.24828 C1222.56419,1052.99589 1222.973,1052.99589 1223.22541,1053.24828 C1223.47911,1053.50067 1223.47911,1053.91075 1223.22541,1054.16314 L1215.45782,1061.92749 C1215.20542,1062.17987 1214.79531,1062.17987 1214.5429,1061.92749 L1214.54279,1061.92804 Z M1225.36584,1064.70579 C1225.7166,1064.70579 1226,1065.00606 1226,1065.3529 C1226,1065.71013 1225.72182,1066 1225.36584,1066 L1204.63544,1065.99978 C1204.28468,1065.99978 1204,1065.69952 1204,1065.35268 C1204,1064.99544 1204.27818,1064.70558 1204.63544,1064.70558 L1225.36584,1064.70579 Z" id="Shape"></path>
+								</g>
+							</svg>
+							<span class="ps-4 fs-6">EN</span>
+						</button>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 
@@ -36,20 +121,8 @@
 		<div class="col-12 col-md-6">
 
 			<div class="row d-flex justify-content-start align-items-center">
-
 				<div class="col-auto">
-					<div class="btn-group mt-2 productCounter" role="group">
-						<button type="button text-secondary" class="btn btn-decrement btn-outline-secondary px-3 py-2 bg-light text-body border-0">-</button>
-						<button type="button" class="btn counter btn-outline-secondary disabled px-4 py-2 bg-light text-body border-0">1</button>
-						<button type="button" class="btn btn-increment btn-outline-secondary px-3 py-2 bg-light text-body border-0">+</button>
-					</div>
-				</div>
-
-				<div class="col-auto flex-grow-1">
-					<div class="btn-group" role="group">
-						<button type="button" class="btn btn-warning py-2 text-body border-0 fw-bold w-100 addToCart" data-productId="123" data-bs-toggle="modal" data-bs-target="#cartModal"> <img src="assets/img/cart.svg" class="pe-4">Pievienot grozam</button>
-						<button type="button" class="btn btn-light text-body toggleWishlist" data-productId="123"><div class="iconContainer" style="height:24px;width:24px"></div></button>
-					</div>
+					<!-- Downloads here -->
 				</div>
 			</div>
 
@@ -59,8 +132,40 @@
 	<div class="row mt-5">
 		<div class="col-md-6">
 			<h5 class="fw-bold text-uppercase mb-4">Kopējais raksturojums</h5>
-			<p><span class="text-secondary">Plastmasas biezums:	<span class="text-body float-end">10 mm</span></span></p>
-			<p><span class="text-secondary">Metāla biezums:	<span class="text-body float-end">2 mm</span></span></p>
+			<p class="mt-4 text-secondary">SIA LLP ir radījis produktu APS (Abrazīvā Cauruļu Sistēma).</p>
+			<p class="mt-4 text-secondary">APS sistēma ir vienkārši un ērti uzstādāma. APS caurules kalpos desmit reizes ilgāk par metāla graudu caurplūdes caurulēm.</p>
+			<p class="mt-4 text-secondary">APS sauklis ir “Gara mūža caurules”, jo pēc laika, kad plastmasa ir izdilusi, jānomaina ir tikai pati plastmasa, nevis viss komplekts.</p>
+			<p class="mt-4 text-secondary">Plastmasas nomaiņa ir tik vienkārša, ka klients to var izdarīt pats un bez speciālām palīgierīcēm.</p>
+		</div>
+		<div class="col-md-6">
+			<h5 class="fw-bold text-uppercase mb-4">Parametri</h5>
+			<p class="border-bottom"><span class="text-secondary">Plastmasas biezums:	<span class="text-body float-end">10 mm</span></span></p>
+			<p class="border-bottom"><span class="text-secondary">Metāla biezums:	<span class="text-body float-end">2 mm</span></span></p>
+			<!--<p class="mt-4 text-body">Noliktavas atlikums:  <span class="text-body fw-bold">12</span></p>-->
+			<h5 class="fw-bold text-uppercase mb-4 mt-4">Darba leņķis</h5>
+			<table class="text-secondary table table-hover table-borderless">
+				<tr class="">
+					<td>&nbsp;</td>
+					<td>0°</td>
+					<td>30°</td>
+					<td>45°</td>
+					<td>90°</td>
+				</tr>
+				<tr>
+					<td>Ø102mm</td>
+					<td>18 t/h</td>
+					<td>14 t/h</td>
+					<td>12 t/h</td>
+					<td>3 t/h</td>
+				</tr>
+				<tr>
+					<td>Ø152mm</td>
+					<td>40 t/h</td>
+					<td>30 t/h</td>
+					<td>25 t/h</td>
+					<td>9 t/h</td>
+				</tr>
+			</table>
 		</div>
 	</div>
 
